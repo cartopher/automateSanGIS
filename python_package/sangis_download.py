094afce220aab57aecfba59b040c2ab87a4abbf2
Christopher J. Charles-Noriega <89684179+cartopher@users.noreply.github.com>
2022-09-28 14:19:48 -0700

    Update sangis_download.py

--- python_package/sangis_download.py
+++ python_package/sangis_download.py
@@ -1,79 +1,141 @@
-#  import all of the commands in commands.py and use them directly from Python:
-from twill.commands import *
-# to talk to the web browser directly, import the browser object:
-from twill import browser
-# import operating system and time packages to create a time-stamped folder
+#  import all packages
 import os
 import time
-# import the zipfile package to to extract the ZIP file contents
 import zipfile
+from twill.commands import *
+from twill import browser
 
 # import the sangis_credentials script to login to the SanGIS website
 import sangis_credentials
 
-# assign a directory path
-# adjust the front end of the path to your preference
-# ensure '\\sangis_package\\output\\Parcels\\'  is the end-point of the directory
-directoryPath = 'C:\\Users\\cchar\\PycharmProjects\\pythonProject\\sangis_package\\output\\Parcels\\'
 
-# store the current year and date into a variable
-currentMonth = time.strftime("%Y_%m")
+# create a sangis_parcels Class Object
+class sangis_parcels():
+    """encapsulate all methods and variables into a Class object"""
 
-# create a time-stamped folder and in the directory location
-if os.path.isdir(directoryPath + currentMonth):
-    print("Directory already exists")
-else:
-    print("Creating directory for you")
-    os.mkdir(directoryPath + currentMonth)
+    # assign class variables
+    directory = None
+    filename = None
+    current_month_folder = None
 
-# change the working directory to the time-stamped folder
-os.chdir(directoryPath + currentMonth)
+    # create constructor and set parameters
+    def __init__(self, directory, filename):
+        """constructor creates class variables and methods
+        :param self: pass 'self' to access variables coming from the constructor
+        :param directory: a file path for all output files
+        :param filename: the name of output files
+        :param current_month_folder: a file path to the time-stamped folder
+        """
 
-# assign a the new directory to the time-stamped folder
-directory = os.getcwd()
+        # initialize class variables
+        self.directory = directory
+        self.filename = filename
 
-# talk to the web browser directly
-go('https://rdw.sandag.org/Account/Login')
-showforms()
+    def changeDirectory(self):
+        """Description:"""
 
-# input login credentials
-fv("1", "ctl00$MainContent$Email", sangis_credentials.username)
-fv("1", "ctl00$MainContent$Password", sangis_credentials.password)
-submit('0')
+        directoryPath = self.directory
 
-# list of the all the ZIP files needed for download from Data Warehouse
-"""
-Assessor_Book.zip
-LANDBASE_BOUNDARIES.zip
-Lots.zip
-Parcels.zip
-PARCELS_EAST.zip
-Parcels_North.zip
-PARCELS_SOUTH.zip
-"""
+        # store the current year and date into a variable
+        currentMonth = time.strftime("%Y_%m")
 
-# navigate to the parcels download page and initiate the download process
-go("gisdtview.aspx?dir=Parcel")
-go("GetFSFile.aspx?dir=Parcel&Name=PARCELS_SOUTH.zip.zip")
+        # create a time-stamped folder and in the directory location
+        if os.path.isdir(directoryPath + currentMonth):
+            print("Directory already exists")
+        else:
+            print("Creating directory for you")
+            os.mkdir(directoryPath + currentMonth)
 
-# open file for writing in binary format
-# overwrite the file if it exists
-# if the file does not exist, create new file for writing
-with open('PARCELS_SOUTH.zip', "wb") as bf:
-    bf.write(browser.dump)
+        # change the current working directory to the time-stamped folder
+        os.chdir(directoryPath + currentMonth)
 
-# navigate to the ZIP file location
-myzip = zipfile.ZipFile(
-    'C:/Users/cchar/PycharmProjects/pythonProject/sangis_package/output/Parcels/2022_08/PARCELS_SOUTH.zip', 'r')
+        # assign the current working directory to the time-stamped folder
+        directory = os.getcwd()
 
-# extract ZIP file into a new folder directory, called 'Current', inside the 'Parcels" folder, called ' by specifying the folder name at the path end-point
-myzip.extractall('C:/Users/cchar/PycharmProjects/pythonProject/sangis_package/output/Parcels/Current')
+        self.current_month_folder = directory
 
-# close the ZIP file
-myzip.close()
+        print(directory)
 
+    def login(self):
+        """Description:"""
+
+        # talk to the web browser directly
+        go('https://rdw.sandag.org/Account/Login')
+        showforms()
+
+        # input login credentials
+        fv("1", "ctl00$MainContent$Email", sangis_credentials.username)
+        fv("1", "ctl00$MainContent$Password", sangis_credentials.password)
+        submit('0')
+
+    def downloadZippedFile(self):
+        """Description:"""
+
+        # navigate to the parcels download page and initiate the download process
+        go("gisdtview.aspx?dir=Parcel")
+        go("GetFSFile.aspx?dir=Parcel&Name=" + self.filename)
+
+        # open file for writing in binary format
+        # overwrite the file if it exists
+        # if the file does not exist, create new file for writing
+        with open(self.filename, "wb") as bf:
+            bf.write(browser.dump)
+
+    def extractZippedFile(self):
+        """Description:"""
+
+        myzip = zipfile.ZipFile(
+            self.current_month_folder + "\\" + self.filename, 'r')
+
+        myzip.extractall(self.directory + "Current")
+
+        # close the ZIP file
+        myzip.close()
+
+    def process_sangis(self):
+        """Description:"""
+
+        # call each method within each try, catch and exception
+        try:
+            self.changeDirectory()
+        except Exception as e:
+            print("Exception when trying to change directory")
+            print(print(str(e)))
+            return
+
+        try:
+            self.login()
+        except Exception as e:
+            print("Exception when trying to go to the specified URL")
+            print(print(str(e)))
+            return
+
+        try:
+            self.downloadZippedFile()
+        except Exception as e:
+            print("Exception when trying to download zipped file")
+            print(print(str(e)))
+            return
+
+        try:
+            self.extractZippedFile()
+        except Exception as e:
+            print("Exception when trying to extract zipped file")
+            print(print(str(e)))
+            return
+
+
+# define main method
 def main():
-    pass
+    """Description:"""
+    directory1 = 'C:\\Users\\cchar\\OneDrive\\Desktop\\download\\package\\output\\Roads\\'
+    download1 = sangis_parcels(directory1, "Assessor_Book.zip")
+    download1.process_sangis()
+
+    directory2 = 'C:\\Users\\cchar\\OneDrive\\Desktop\\download\\package\\output\\Roads\\'
+    download2 = sangis_parcels(directory2, "PARCELS_EAST.zip")
+    download2.process_sangis()
+
 
 if __name__ == '__main__':
     main()
